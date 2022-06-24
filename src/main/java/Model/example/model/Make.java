b4258809c3bb1818d20f645a1b2e7a91d770cd90
Alex <asacaquirin17@gmail.com>
2022-06-24 02:49:31 -0400

    Last changes

--- src/main/java/Model/example/model/Make.java
+++ src/main/java/Model/example/model/Make.java
@@ -1,15 +1,2 @@
-package Model.example.model;
-
-public enum Make {
-    JEEP("JEEP"), Jeep("Jeep"),
-    HONDA("HONDA"), Honda("Honda"),
-    BMW("BMW"), Bmw("Bmw"),
-    FORD("FORD"),Ford("Ford");
-
-    public final String value;
-
-    Make(String value){
-        this.value = value;
-    }
-
+package Model.example.model;public class Make {
 }
